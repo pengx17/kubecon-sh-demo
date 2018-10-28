debb0fb9e4506926f0ad461562b5ba6a41a14e51
Peng Xiao <pengxiao@alauda.io>
2018-10-28 11:38:24 +0800

    feat: add build for *nix

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf dist
+echo ">>> Building kubecon-sh-demo:build"
+docker build -t pengxiao/kubecon-demo:build -f ./dockerfiles/Dockerfile.build .
+docker container create --name kubecon-demo-build-extract pengxiao/kubecon-demo:build
+docker container cp kubecon-demo-build-extract:/dist ./dist
+docker container rm kubecon-demo-build-extract
+
+echo ">>> Building kubecon-sh-demo:latest"
+docker build --no-cache -t pengxiao/kubecon-demo:latest -f ./dockerfiles/Dockerfile .
+rm -rf dist
