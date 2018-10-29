24b2a5d77fa5de3ebfd28c0d42e215a362dcc676
Peng Xiao <pengxiao@outlook.com>
2018-10-29 16:43:00 +0800

    feat: add mockups with  env control

--- server/main.go
+++ server/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 // loggingHandler enhances a handler with access logging
@@ -15,10 +17,16 @@ func (h loggingHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	h.handler.ServeHTTP(w, req)
 }
 
+func selectModeFromEnv(w http.ResponseWriter, req *http.Request) {
+	mode := os.Getenv("SH_DEMO_APP_MODE")
+	fmt.Fprintf(w, mode)
+}
+
 func main() {
 	fs := http.FileServer(http.Dir("."))
+	http.HandleFunc("/api/mode", selectModeFromEnv)
 	http.Handle("/", loggingHandler{fs})
 
-	log.Printf("Serving at HTTP port 8080")
-	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
+	log.Printf("Serving at HTTP port 1216")
+	log.Fatal(http.ListenAndServe("0.0.0.0:1216", nil))
 }
--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+import newMain from "./k8s-store.png";
+import oldMain from "./k8s-store-old.png";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { url: undefined };
+  }
+  componentDidMount() {
+    fetch("/api/mode")
+      .then(res => res.text())
+      .then(res => {
+        const url = res.toLowerCase() === "new" ? newMain : oldMain;
+        this.setState({ url });
+      });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.url && <img src={this.state.url} alt="main" />}
       </div>
     );
   }
